115990e1d3afea98e50338efc4887ae68e864041
Webml-Community <hw@unhooked.uk>
2026-03-18 01:18:04 +0000

    Delete src directory

--- src/theme/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from "./button/Button";
-export { default as Textarea } from "./form/Textarea";
-export { default as Loader } from "./misc/Loader";
-export { default as Card } from "./misc/Card";
